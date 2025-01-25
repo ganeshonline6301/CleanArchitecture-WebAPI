e39827aa6b9b21230a6ced7f09de7a54c521ae70
Ganesh Sri Surya <robinganesh007@gmail.com>
2025-01-26 00:29:55 +0530

    Domain entities created

---
+++ src/CleanArch.Domain/Tasks/Enums/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.SmartEnum;
+
+namespace CleanArch.Domain.Tasks.Enums;
+
+public class TaskStatus : SmartEnum<TaskStatus>
+{
+    public TaskStatus(string name, int value) : base(name, value)
+    {
+    }
+    
+    public static readonly TaskStatus Completed = new(nameof(Completed), 1);
+    public static readonly TaskStatus InProgress = new(nameof(InProgress), 2);
+    public static readonly TaskStatus NotStarted = new(nameof(NotStarted), 3);
+    
+}
---
+++ src/CleanArch.Domain/Tasks/Task.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Common;
+
+namespace CleanArch.Domain.Tasks;
+
+public class Task : Entity
+{
+    public string Title { get; }
+    public string Description { get; }
+    public DateTime DueDate { get; }
+    public Guid UserId { get; }
+
+    public Task(string title, string description, DateTime dueDate, Guid userId, Guid? id = null) : base(id ?? Guid.NewGuid())
+    {
+        Title = title;
+        Description = description;
+        DueDate = dueDate; 
+        UserId = userId;
+    }
+
+    private Task()
+    {
+        
+    }
+}
