8e582a851bd6e59ebc699c93d873508f482a2d46
Ganesh Sri Surya <robinganesh007@gmail.com>
2025-01-26 22:43:45 +0530

    Implemented Query handler for ToDo Entity

---
+++ src/CleanArch.Application/Tasks/Queries/GetToDo/GetToDoQuery.cs
@@ -0,0 +1,8 @@
+﻿using CleanArch.Domain.ToDos;
+using MediatR;
+using ErrorOr;
+
+namespace CleanArch.Application.Tasks.Queries.GetToDo;
+
+public record GetToDoQuery(
+    Guid Id) : IRequest<ErrorOr<ToDo>>;
