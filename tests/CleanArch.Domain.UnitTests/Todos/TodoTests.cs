9355d6efa2d0dd630ec1511e777462b8727833e7
Ganesh Sri Surya <robinganesh007@gmail.com>
2025-02-04 23:54:57 +0530

    Created UnitTest project for Domain layer

---
+++ tests/CleanArch.Domain.UnitTests/Todos/TodoTests.cs
@@ -0,0 +1,20 @@
+﻿using TestCommon.Todos;
+
+namespace CleanArch.Domain.UnitTests.Todos;
+
+public class TodoTests
+{
+    [Fact]
+    public void AddTodo()
+    {
+        // Arrange
+        var todo = TodoFactory.CreateTodo();
+        
+        
+        // Act
+        
+        
+        
+        // Assert
+    }
+}
---
+++ tests/TestCommon/Todos/TodoFactory.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.ToDos;
+using CleanArch.Domain.ToDos.Enums;
+using TestCommon.TestConstants;
+
+namespace TestCommon.Todos;
+
+public static class TodoFactory
+{
+    public static ToDo CreateTodo(
+        ToDoPriority? priority = null,
+        ToDoStatus? status = null,
+        Guid? userId = null,
+        Guid? id = null)
+    {
+        return new ToDo(
+            title: Constants.Todos.Title,
+            description: Constants.Todos.Description,
+            dueDate: Constants.Todos.DueDate,
+            priority: priority ?? Constants.Todos.DefaultPriority,
+            status: status ?? Constants.Todos.DefaultStatus,
+            userId: userId ?? Constants.Todos.UserId,
+            id: id ?? Constants.Todos.Id);
+    }
+}
